c506cc092a4c5a3c082a3f95e3d29ee15711ed4c
Mohammad Sarya Tulaimat <sarya@tabtabgo.com>
2024-05-21 15:31:48 +0300

    notification storage

---
+++ server/src/TabTabGo.WebStream/NotificationStorage/TabTabGo.WebStream.NotificationStorage/Entites/NotificationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TabTabGo.WebStream.NotificationStorage.Entites.Enums;
+
+namespace TabTabGo.WebStream.NotificationStorage.Entites
+{
+
+    public class NotificationUser
+    {
+        public string UserId { get; set; }
+        public NotificationUserStatus Status { get; set; } = NotificationUserStatus.Unread;
+        public DateTime NotidyDateTime { get; set; } = DateTime.UtcNow;
+        public DateTime? ReadDateTime { get; set; }
+        public Guid NotificationId { get; set; }
+        public Notification Notification { get; set; }
+    }
+}
---
+++ server/src/TabTabGo.WebStream/NotificationStorage/TabTabGo.WebStream.NotificationStorage/Repository/INotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TabTabGo.WebStream.NotificationStorage.Entites;
+
+namespace TabTabGo.WebStream.NotificationStorage.Repository
+{
+    public interface INotificationRepository
+    {
+        Guid Create(Notification notification);
+        Notification Find(Guid Id);
+        List<Notification> FindByUserId(string UserId); 
+        Task<Guid> CreateAsync(Notification notification, CancellationToken cancellationToken = default);
+        Task<Notification> FindAsync(Guid Id, CancellationToken cancellationToken = default);
+        Task<List<Notification>> FindByUserIdAsync(string UserId, CancellationToken cancellationToken = default); 
+    }
+}
