27022ec876ed68d769964bd97827904124e2c3f2
Mohammad Sarya Tulaimat <sarya@tabtabgo.com>
2024-05-22 10:02:37 +0300

    add CompositPushEvent

--- server/src/TabTabGo.WebStream/TabTabGo.WebStream/Builders/PushEventBuilders/PushEventBuilder.cs
+++ server/src/TabTabGo.WebStream/TabTabGo.WebStream/Builders/PushEventBuilders/PushEventBuilder.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TabTabGo.WebStream.Services.Contract;
+using TabTabGo.WebStream.Services.PushEventsServices;
 
 namespace TabTabGo.WebStream.Builders.PushEventBuilders
 {
     public class PushEventBuilder
     {
-        Func<IServiceProvider, IPushEvent> pushBuilder;
-        public PushEventBuilder SetIPushEvent(Func<IServiceProvider, IPushEvent> func)
+        private readonly List<Func<IServiceProvider, IPushEvent>> _pushBuilder = new List<Func<IServiceProvider, IPushEvent>>();
+        public PushEventBuilder AddPushEvent(Func<IServiceProvider, IPushEvent> func)
         {
-            pushBuilder = func;
+            _pushBuilder.Add(func);
             return this;
         }
         public IPushEvent Build(IServiceProvider serviceProvider)
         {
-            return pushBuilder(serviceProvider); 
+            return new CompositPushEvent(_pushBuilder.Select(x => x(serviceProvider)));
         }
     }
 }
