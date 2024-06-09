1ac34beb41a062b93a54607c1915ad132180a7de
Mohammad Sarya Tulaimat <sarya@tabtabgo.com>
2024-06-09 14:43:46 +0300

    fix save issues

--- server/test/TabTabGo.WebStream.TestApp/UnitOfWork.cs
+++ server/test/TabTabGo.WebStream.TestApp/UnitOfWork.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
 using TabTabGo.Core.Data;
 
 namespace TabTabGo.WebStream.TestApp
 {
     public class UnitOfWork : TabTabGo.Data.EF.UnitOfWork, IUnitOfWork
     {
+        DbContext? _context;
         public UnitOfWork(DbContext? context, ILogger<TabTabGo.Data.EF.UnitOfWork> logger) : base(context, logger)
         {
+            _context = context;
         }
 
         public override void Dispose()
         {
-            
+             
         }
     }
 }
